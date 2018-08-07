1aa3c296a3c41f8051d532bb688d130b18772117
sivakrishnamullapudi <40879805+sivakrishnamullapudi@users.noreply.github.com>
2018-08-07 10:53:30 +0530

    Update StorySteps.java

--- Project_xl/src/com/cg/tests/StorySteps.java
+++ Project_xl/src/com/cg/tests/StorySteps.java
@@ -48,7 +48,7 @@ public class StorySteps {
 	public void registration_Page_is_loaded() throws Throwable {
 		System.setProperty("webdriver.chrome.driver", "D:\\Users\\simullap\\Documents\\chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.get("D:\Users\simullap\Documents\inputFromXLFileSelenium-master\inputFromXLFileSelenium-master\ProjRegistration_xl\Pages\RegistrationPage.html");
+		driver.get("D:\BDD_workspace\inputFromXLFileSelenium-master\ProjRegistration_xl\Pages\RegistrationPage.html");
 
 	}
 
